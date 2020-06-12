bbc89e32a152754620c212268eb026135db81630
wonjin0217 <yssung021@gmail.com>
2020-06-13 03:57:14 +0900

    Skill 7 : 포물선 발사체
    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    - Visual Studio 버전 : Visual Community 2017

---
+++ skillPractice/Assets/_SWJ/2. Scripts/Arrow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.right = GetComponent<Rigidbody2D>().velocity;
+    }
+}
