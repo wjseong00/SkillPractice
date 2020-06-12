2fb769ce3fabc9f4302f03a6d3ef7dc1792c297a
wonjin0217 <yssung021@gmail.com>
2020-06-13 00:32:13 +0900

    Skill 1 : 오브젝트 풀링
    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    - Visual Studio 버전 : Visual Community 2017

---
+++ skillPractice/Assets/_SWJ/2. Scripts/CreateCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreateCoroutine());
+    }
+    IEnumerator CreateCoroutine()
+    {
+        while(true)
+        {
+            yield return null;
+            GameObject t_object = ObjectPoolingManager.instance.GetQueue();
+            t_object.transform.position = Vector3.zero;
+                
+        }
+    }
+
+}
