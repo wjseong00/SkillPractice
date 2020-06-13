9e645f430116e64764279628c04cb1f04213c7fa
wonjin0217 <yssung021@gmail.com>
2020-06-13 13:19:49 +0900

    Skill 10 : 유도 미사일
    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    - Visual Studio 버전 : Visual Community 2017

---
+++ skillPractice/Assets/_SWJ/2. Scripts/MissileLauncher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLauncher : MonoBehaviour
+{
+    [SerializeField] GameObject m_goMissile = null;
+    [SerializeField] Transform m_tfMissileSpawn = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject t_missile = Instantiate(m_goMissile, m_tfMissileSpawn.position, Quaternion.identity);
+            t_missile.GetComponent<Rigidbody>().velocity = Vector3.up * 5f;
+        }
+    }
+}
